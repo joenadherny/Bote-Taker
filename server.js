fc8568c4929909720355263e19eba0dc63f2dd5f
Joe Nadherny <joenadherny@gmail.com>
2023-12-03 17:33:30 -0700

    finally got server to work but functions won't

--- server.js
+++ server.js
@@ -1,59 +1,47 @@
 // const express = require('express');
+// const path = require('path');
 // const fs = require('fs');
-// const { v4: uuidv4 } = require('uuid');
 
 // const app = express();
-// const PORT = process.env.PORT || 3001;
-// const path= require('path');
+// const PORT = process.env.PORT || 3000;
+
+// // Middleware
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
-// app.use(express.static('develop/public'));
+// app.use(express.static('public'));
 
-// // HTML Routes
-
-// // Route to display the "Notes" page
-// app.get('/notes', (req, res) => {
-//   res.sendFile(__dirname + 'develop', 'public', 'notes.html');
-// });
-
-// // Route to display the homepage
+// // Routes
 // app.get('/', (req, res) => {
-//   res.sendFile(__dirname + 'develop', 'public', 'index.html');
+//   res.sendFile(path.join(__dirname,'develop', 'public', 'index.html'));
 // });
 
-// // API Routes
+// app.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'develop', 'public', 'note.html'));
+// });
 
-// // Get the list of notes
 // app.get('/api/notes', (req, res) => {
-//   const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf8'));
+//   const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 //   res.json(notes);
 // });
 
-// // Create a new note
 // app.post('/api/notes', (req, res) => {
 //   const newNote = req.body;
-//   newNote.id = uuidv4();
-//   const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf8'));
+//   const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+//   newNote.id = notes.length + 1;
 //   notes.push(newNote);
-//   fs.writeFileSync('./Develop/db/db.json', JSON.stringify(notes, null, 2)); 
+//   fs.writeFileSync('db/db.json', JSON.stringify(notes));
 //   res.json(newNote);
 // });
 
-// // Delete a note by its ID
 // app.delete('/api/notes/:id', (req, res) => {
-//   const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf8')); 
-//   const noteId = req.params.id;
-
-//   // Filter out the notes to be deleted
-//   const updatedNotes = notes.filter((note) => note.id !== noteId);
-
-//   // Write the updated notes to db.json
-//   fs.writeFileSync('./Develop/db/db.json', JSON.stringify(updatedNotes, null, 2)); 
-
-//   res.json({ message: 'Note deleted' });
+//   const noteId = parseInt(req.params.id);
+//   let notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+//   notes = notes.filter((note) => note.id !== noteId);
+//   fs.writeFileSync('db/db.json', JSON.stringify(notes));
+//   res.json({ success: true });
 // });
 
-// // Start the server
+// // Start server
 // app.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
 // });
@@ -70,19 +58,26 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Routes
+
+// Home route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname,'develop', 'public', 'index.html'));
 });
 
+// Notes route
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/note.html'));
+  res.sendFile(path.join(__dirname, 'develop', 'public', 'note.html'));
 });
 
+// API Routes
+
+// GET notes route
 app.get('/api/notes', (req, res) => {
   const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
   res.json(notes);
 });
 
+// POST new note route
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
   const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
@@ -92,6 +87,7 @@ app.post('/api/notes', (req, res) => {
   res.json(newNote);
 });
 
+// DELETE note route
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = parseInt(req.params.id);
   let notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
